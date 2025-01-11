51bae9f27cf75e17a3147c8e03a899b0fbd2495a
Jhonathan <jhonathanps46@gmail.com>
2025-01-11 16:49:05 -0500

    commit inicial

---
+++ src/test/java/com/backend/reactivo/app/PruebaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.reactivo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
